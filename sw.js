08c8359a05d40b42889728cd7a3a7931d867eba9
Malcolm Morgan <mem48@users.noreply.github.com>
2024-07-15 22:54:04 +0100

    New Service Worker

--- sw.js
+++ sw.js
@@ -1,98 +1,53 @@
-const staticCacheName = 'site-static-v3';
-const assets = [
-  '/about/index.html',
-  '/about/faq/index.html',
-  '/data/index.html',
-  '/css/homepage.css',
-  '/css/main.css',
-  '/css/manual.css',
-  '/css/map.css',
-  '/js/common-nonmap.js',
-  '/js/datasets-common.js',
-  '/js/manual.js',
-  '/js/parallax.js',
-  '/js/settings-common.js',
-  '/js/ui-common.js',
-  '/landownership/index.html',
-  '/landownership/datsets.js',
-  '/landownership/settings.js',
-  '/landownership/ui.js',
-  '/landuse/index.html',
-  '/landuse/datsets.js',
-  '/landuse/ui.js',
-  '/legacy/index.html',
-  '/manual/index.html',
-  '/manual/index.md',
-  '/privacy/index.html',
-  '/tiles/partial-style_oszoom_names.json',
-  '/tiles/style_dark_nobuild.json',
-  '/tiles/style_google_nobuild.json',
-  '/tiles/style_greyscale_nobuild.json',
-  '/tiles/style_opencyclemap.json',
-  '/tiles/style_satellite.json',
-  '/transport/index.html',
-  '/transport/datsets.js',
-  '/transport/settings.js',
-  '/transport/ui.js',
-  '/transport/style.css'
-];
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-// install event
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
+const CACHE = "pwabuilder-offline-page";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const offlineFallbackPage = "offline.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-const dynamicCacheName = 'site-dynamic-v1';
+        if (preloadResp) {
+          return preloadResp;
+        }
 
-// activate event
-self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key =>  key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-});
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
 
-// fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    })
-  );
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
