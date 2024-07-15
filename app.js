622f472e48716439e884314a22ee8b4d9288e14f
Malcolm Morgan <mem48@users.noreply.github.com>
2024-07-15 23:01:13 +0100

    retry SW

--- app.js
+++ app.js
@@ -1,52 +1,7 @@
-const CACHE_NAME = 'CAP-cache';
-
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [
-    '/about/index.html',
-    '/about/faq/index.html',
-    '/data/index.html',
-    '/css/homepage.css',
-    '/css/main.css',
-    '/css/manual.css',
-    '/css/map.css',
-    '/js/common-nonmap.js',
-    '/js/datasets-common.js',
-    '/js/manual.js',
-    '/js/parallax.js',
-    '/js/settings-common.js',
-    '/js/ui-common.js',
-    '/landownership/index.html',
-    '/landownership/datsets.js',
-    '/landownership/settings.js',
-    '/landownership/ui.js',
-    '/landuse/index.html',
-    '/landuse/datsets.js',
-    '/landuse/ui.js',
-    '/legacy/index.html',
-    '/manual/index.html',
-    '/manual/index.md',
-    '/privacy/index.html',
-    '/tiles/partial-style_oszoom_names.json',
-    '/tiles/style_dark_nobuild.json',
-    '/tiles/style_google_nobuild.json',
-    '/tiles/style_greyscale_nobuild.json',
-    '/tiles/style_opencyclemap.json',
-    '/tiles/style_satellite.json',
-    '/transport/index.html',
-    '/transport/datsets.js',
-    '/transport/settings.js',
-    '/transport/ui.js',
-    '/transport/style.css'
-]
-
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
-    })());
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err));
+  });
+}
--- sw.js
+++ sw.js
@@ -1,5 +1,57 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+const CACHE_NAME = 'CAP-cache';
 
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = [
+    '/about/index.html',
+    '/about/faq/index.html',
+    '/data/index.html',
+    '/css/homepage.css',
+    '/css/main.css',
+    '/css/manual.css',
+    '/css/map.css',
+    '/js/common-nonmap.js',
+    '/js/datasets-common.js',
+    '/js/manual.js',
+    '/js/parallax.js',
+    '/js/settings-common.js',
+    '/js/ui-common.js',
+    '/landownership/index.html',
+    '/landownership/datsets.js',
+    '/landownership/settings.js',
+    '/landownership/ui.js',
+    '/landuse/index.html',
+    '/landuse/datsets.js',
+    '/landuse/ui.js',
+    '/legacy/index.html',
+    '/manual/index.html',
+    '/manual/index.md',
+    '/privacy/index.html',
+    '/tiles/partial-style_oszoom_names.json',
+    '/tiles/style_dark_nobuild.json',
+    '/tiles/style_google_nobuild.json',
+    '/tiles/style_greyscale_nobuild.json',
+    '/tiles/style_opencyclemap.json',
+    '/tiles/style_satellite.json',
+    '/transport/index.html',
+    '/transport/datsets.js',
+    '/transport/settings.js',
+    '/transport/ui.js',
+    '/transport/style.css'
+]
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -51,3 +103,4 @@ self.addEventListener('fetch', (event) => {
     })());
   }
 });
+
