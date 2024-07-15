de05d1b47a160fa513f3c23214196d1f0734ae40
Malcolm Morgan <mem48@users.noreply.github.com>
2024-07-15 18:39:31 +0100

    Add install button to homepage

--- js/parallax.js
+++ js/parallax.js
@@ -16,3 +16,27 @@ function enableParallax() {
 document.body.onload = enableParallax();
 
 
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+
+
